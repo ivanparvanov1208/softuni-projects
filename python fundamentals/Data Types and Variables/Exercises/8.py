b887d27c0de2e9e0c9c876207fa5b212190010a6
ivanparvanov1208 <ivan4o54783@gmail.com>
2025-05-23 22:28:12 +0300

    .

--- python fundamentals/Data Types and Variables/Exercises/8.py
+++ python fundamentals/Data Types and Variables/Exercises/8.py
@@ -1,4 +1,4 @@
-group_size = int(input())
+groupSize= int(input())
 days = int(input())
 
 coins_earned = 0
@@ -6,25 +6,25 @@ coins_earned = 0
 for current_day in range(1, days + 1):
 
     if current_day % 10 == 0:
-        group_size -= 2
+        groupSize -= 2
 
     if current_day % 15 == 0:
-        group_size += 5
+        groupSize += 5
 
-    food_for_the_companions = 2 * group_size
+    food_for_the_companions = 2 * groupSize
     daily_earnings = 50 - food_for_the_companions
     coins_earned += daily_earnings
 
     if current_day % 3 == 0:
-        coins_earned -= 3 * group_size
+        coins_earned -= 3 * groupSize
 
     if current_day % 5 == 0:
-        coins_earned += 20 * group_size
+        coins_earned += 20 * groupSize
 
         if current_day % 3 == 0:
-            coins_earned -= 2 * group_size
+            coins_earned -= 2 * groupSize
 
-coins_per_companion = int(coins_earned / group_size)
+coins_per_companion = int(coins_earned / groupSize)
 
-print(f"{group_size} companions received {coins_per_companion} coins each.")
+print(f"{groupSize} companions received {coins_per_companion} coins each.")
 
