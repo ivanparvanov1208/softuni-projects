62b0641cf4a4b099340e0cd3fefe92bd586c6667
Ivan Parvanov <ivan4o54783@gmail.com>
2025-11-01 10:41:07 +0200

    Updating the latest release

--- python advanced/OOP/Objects and Classes/7/main.py
+++
@@ -1,7 +0,0 @@
-from project.album import Album
-from project.song import Song
-from project.band import Band
-
-band = Band("Death")
-album = Album("The Sound of Perseverance")
-print(band.remove_album("The Sound of Perseverance"))
